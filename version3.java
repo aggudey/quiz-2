01e278674ae9feebaa50c5daa67674eb5b795cd5
aggudey <aggudeysamuel7@gmail.com>
2026-02-09 04:24:19 +0800

    Add files via upload

---
+++ version3.java
@@ -0,0 +1,11 @@
+
+
+public class version3 {
+    public static double calculateClassAverage(double[] scores) {
+        double sum = 0.0;
+        for (double score : scores) {
+            sum += score;
+        }
+        return sum / scores.length;
+    }
+}
